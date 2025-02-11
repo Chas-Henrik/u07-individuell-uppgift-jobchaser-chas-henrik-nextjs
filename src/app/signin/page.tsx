3b9d8e8652a051ee0db309f1e759d9c198b9fd87
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-02-11 14:28:11 +0100

    Adjusted error message alignment in signin form

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -29,12 +29,12 @@ export default function SignIn() {
         <article style={themeStyles} className={styles.signinForm}>
             <h1 className={styles.header}>Login</h1>
             <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-                <input className={styles.formInput} type="email" placeholder="E-mail" {...register("emailRequired", { required: true })}
+                <div><input className={styles.formInput} type="email" placeholder="E-mail" {...register("emailRequired", { required: true })}
                 aria-invalid={errors.emailRequired ? "true" : "false"} />
-                {errors.emailRequired?.type === "required" && (<p className={styles.error} role="alert">E-mail is required</p>)}
-                <input className={styles.formInput} type="password" placeholder="Password" minLength={6} {...register("passwordRequired", { required: true })}
+                {errors.emailRequired?.type === "required" && (<p className={styles.error} role="alert">E-mail is required</p>)}</div>
+                <div><input className={styles.formInput} type="password" placeholder="Password" minLength={6} {...register("passwordRequired", { required: true })}
                 aria-invalid={errors.passwordRequired ? "true" : "false"} />
-                {errors.passwordRequired?.type === "required" && (<p className={styles.error} role="alert">Password is required</p>)}
+                {errors.passwordRequired?.type === "required" && (<p className={styles.error} role="alert">Password is required</p>)}</div>
                 <button className={styles.formSubmitButton} type="submit">Submit</button>
                 <div></div>
             </form>
